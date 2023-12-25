592249aa7c59dd65e834795834f4b7b77b0010ea
TharmsyXD <133595791+TharmsyXD@users.noreply.github.com>
2023-12-25 04:12:54 +0000

    Updated Start Menu

--- src/main/java/play/tharmsy/Start.java
+++ src/main/java/play/tharmsy/Start.java
@@ -1,10 +1,20 @@
 package play.tharmsy;
+
+import javax.swing.JOptionPane;
+import play.tharmsy.UI.Auth.Auth;
 import play.tharmsy.UI.JFrameWindow;
 
 public class Start {
     public static void main(String[] args) {
+        String[] options = { "Auth Window", "Simple Window" };
+        int choice = JOptionPane.showOptionDialog(null, "Select an option:", "Window Selection", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-        System.out.print("Starting App... \n");
-        JFrameWindow.main(args);
+        if (choice == 0) {
+            Auth.main(args);
+        } else if (choice == 1) {
+            JFrameWindow.main(args);
+        } else {
+            System.out.println("Application closed.");
         }
     }
+}
